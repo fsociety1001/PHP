7d0a634195c82fa526d2686020b680ab077766a9
fsociety <143713077+fsociety1001@users.noreply.github.com>
2023-10-09 00:12:09 -0300

    nav.php

---
+++ dox/doxBin/data/nav.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark text-black bg-black mb-5">
+    <div class="container">
+      <a class="navbar-brand" href="#">Produto.</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false">
+        <i class="fa-solid fa-align-left navbar-toggler-icon"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mx-auto mb-2">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Início</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sobre</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Notas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">FAQ</a>
+          </li>
+        </ul>
+        
+      </div>
+    </div>
+  </nav>
